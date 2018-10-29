20424e5faacf43eb9288a8d21b594be394ebc1a3
kehahn <40897508+kehahn@users.noreply.github.com>
2018-10-29 15:46:10 -0700

    Update energy.js

--- energy.js
+++ energy.js
@@ -2,5 +2,7 @@ function outOfEnergy(int energyUnit) {
 	if(energyUnit <= 0) {
 		alert("The hero has run out of energy.");
 		alert("You have lost. Game Over!");
+		return true;
 	}
+	return false;
 }
