949087d985788d454df0f3f7314dca1b23f2d8c4
kehahn <40897508+kehahn@users.noreply.github.com>
2018-10-30 10:55:07 -0700

    Added comments.

--- energy.js
+++ energy.js
@@ -1,3 +1,5 @@
+//This function checks if the hero has run out of energy, and returns a bool.
+//Subject to change depending on how we fit the function into the program/if we want the function to end the game as well.
 function outOfEnergy(int energyUnit) {
 	if(energyUnit <= 0) {
 		alert("The hero has run out of energy.");
