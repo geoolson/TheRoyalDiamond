a4e2825626d838f749cf50aeb18428ede425faab
luke <lcp2@pdx.edu>
2018-10-31 13:10:53 -0700

    De-duplicate movement and checking code. Also modify map display slightly.

--- map.js
+++ map.js
@@ -45,14 +45,21 @@ function Map(width, height, starting_x, starting_y, starting_whiffles, starting_
         }
     }
 
+    this.update = function() {
+        // For a normal step, as long as hero has 
+        // enough energy, they will lose 1 energy.
+        this.hero.decrement_energy();
 
+        //This part will update the text fields with the hero's current location:
+        document.getElementById("location").innerHTML = "<b>Current Location: </b>Coordinates: (" + this.hero_x + ", " + this.hero_y + ")";
+        document.getElementById("map_box").innerHTML = this.map_string();
+    };
 
     //Member Functions:
     //Moves the player north by calling the move_north function of the player class
     this.move_north = function(){
         if(!((this.hero_y + 1) > height)) //If the hero is not at the edge of the map, she can move north
-                                     // But if she is at the edge of the map, then we skip the movement call, 
-                                     // because she can't move north anymore.
+                                     // But if she is at the edge of the map, then we wrap around to the south
         {
             this.hero.move_north();
             this.hero_y = this.hero_y + 1;
@@ -61,42 +68,28 @@ function Map(width, height, starting_x, starting_y, starting_whiffles, starting_
         {   // will be wrapped around to the other side (southmost)
             this.hero_y = 0;
         }
-
-	// For a normal step, as long as hero has 
-	// enough energy, they will lose 1 energy.
-	this.hero.decrement_energy();
-	
-
-        //This part will update the text fields with the hero's current location:
-        document.getElementById("location").innerHTML = "<b>Current Location: </b>Coordinates: (" + this.hero_x + ", " + this.hero_y + ")";
+        
+        this.update();
     };
     //Moves the player sorth by calling the move_south function of the player class
     this.move_south = function(){
         if(!((this.hero_y - 1) < 0)) //If the hero is not at the edge of the map, she can move south
-                                     // But if she is at the edge of the map, then we skip the movement call, 
-                                     // because she can't move south anymore.
+                                     // But if she is at the edge of the map, then we wrap around to the north
         {
             this.hero.move_south();
             this.hero_y = this.hero_y - 1;
         }
         else // If hero gets to edge going south, they
         {   // will be wrapped to the other side (northmost)
-            this.hero_y = height;
+            this.hero_y = height-1;
         }
 
-	// For a normal step, as long as hero has 
-	// enough energy, they will lose 1 energy.
-	this.hero.decrement_energy();
-
-        //This part will update the text fields with the hero's current location:
-        document.getElementById("location").innerHTML = "<b>Current Location: </b>Coordinates: (" + this.hero_x + ", " + this.hero_y + ")";
-
+        this.update();
     };
     //Moves the player east by calling the move_east function of the player class
     this.move_east = function(){
         if(!((this.hero_x + 1) > width)) //If the hero is not at the edge of the map, she can move east
-                                     // But if she is at the edge of the map, then we skip the movement call, 
-                                     // because she can't move east anymore.
+                                     // But if she is at the edge of the map, then we wrap around to the west
         {
             this.hero.move_east();
             this.hero_x = this.hero_x + 1;
@@ -106,84 +99,69 @@ function Map(width, height, starting_x, starting_y, starting_whiffles, starting_
             this.hero_x = 0;
         }
 
-	// For a normal step, as long as hero has 
-	// enough energy, they will lose 1 energy.
-	this.hero.decrement_energy();
-
-
-        //This part will update the text fields with the hero's current location:
-        document.getElementById("location").innerHTML = "<b>Current Location: </b>Coordinates: (" + this.hero_x + ", " + this.hero_y + ")";
-
+        this.update();
     };
     //Moves the player west by calling the move_west function of the player class
     this.move_west = function(){
         if(!((this.hero_x - 1) < 0)) //If the hero is not at the edge of the map, she can move west
-                                         // But if she is at the edge of the map, then we skip the movement call, 
-                                         // because she can't move west anymore.
+                                         // But if she is at the edge of the map, then we wrap around to the east
         {
             this.hero.move_west();
             this.hero_x = this.hero_x - 1;
         }
         else // When hero reaches westmost edge, they
         {    // are wrapped around to the eastmost side.
-            this.hero_x = width;
+            this.hero_x = width-1;
         }
 
-	// For a normal step, as long as hero has 
-	// enough energy, they will lose 1 energy.
-	this.hero.decrement_energy();
-
-        //This part will update the text fields with the hero's current location:
-        document.getElementById("location").innerHTML = "<b>Current Location: </b>Coordinates: (" + this.hero_x + ", " + this.hero_y + ")";
-
+        this.update();
     };
 
     // Formats the map array as the contents of an HTML table.
     this.map_string = function() {
         result = "";
-        for (var i = 0; i < cells.length; ++i) {
-            result += "<tr>";
-            for (var j = 0; j < cells.length; ++j) {
-                var cell = cells[i][j];
-                result += "<td>";
-                    if(cell.isVisible) {
-                        switch(cell.terrain) {
-                        case 0:
-                            // Meadow
-                            result += ".";
-                            break;
-                        case 1:
-                            // Forest
-                            result += ";";
-                            break;
-                        case 2:
-                            // Water
-                            result += "~";
-                            break;
-                        case 3:
-                            // Wall
-                            result += "#";
-                            break;
-                        case 4:
-                            // Bog
-                            result += ",";
-                            break;
-                        case 5:
-                            // Swamp
-                            result += "%";
-                            break;
-                        default:
-                            result += "?";
-                        }
-                    } else {
-                        result += " ";
+        for (var i = 0; i < this.cells.length; ++i) {
+            for (var j = 0; j < this.cells.length; ++j) {
+                var cell = this.cells[i][j];
+                if (i == this.hero_y && j == this.hero_x) {
+                    result += "@";
+                } else if(cell.isVisible) {
+                    switch(cell.terrain) {
+                    case 0:
+                        // Meadow
+                        result += ".";
+                        break;
+                    case 1:
+                        // Forest
+                        result += ";";
+                        break;
+                    case 2:
+                        // Water
+                        result += "~";
+                        break;
+                    case 3:
+                        // Wall
+                        result += "#";
+                        break;
+                    case 4:
+                        // Bog
+                        result += ",";
+                        break;
+                    case 5:
+                        // Swamp
+                        result += "%";
+                        break;
+                    default:
+                        result += "?";
                     }
-                result += "</td>";
+                } else {
+                    result += "-";
+                }
             }
-            result += "</tr>"
+            result += "<br>";
         }
         return result;
-    }
+    };
 
 }
 
