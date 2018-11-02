de003e15991e206fec344310fdb788d5f323a2c4
geoolson <geoolson@pdx.edu>
2018-11-01 20:54:01 -0700

    fixed loading issues and now parses the file

--- loadMap.js
+++ loadMap.js
@@ -1,5 +1,92 @@
-function loadMap()
-{
-    var read = new FileReader();
-    read.readAsText();
+
+function loadFileAsText(){
+    var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+    var fileReader = new FileReader( fileToLoad );
+    fileReader.onload = function(fileLoadedEvent){
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+    dimensions(fileReader.result);
 }
+
+var openFile = function(event){
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+        var text = reader.result;
+        dimensions(text);
+    }
+    reader.readAsText(input.files[0]);
+}
+
+function dimensions(file){
+    var pattern = /[0-9]+/;
+    var result = pattern.exec(file);
+    alert("dimensions" + result);
+    var str = file.substr(result.index + result[0].length, file.length);
+    if(result)
+    playerLocation(str);
+}
+
+function playerLocation(file){
+    //x position
+    var pattern = /[0-9]+/;
+    var result = pattern.exec(file);
+    var str = file.substr(result.index + result[0].length, file.length);
+    alert("x coordinate" + result);
+
+    //y position
+    pattern = /[0-9]+/;
+    result = pattern.exec(str);
+    str = str.substr(result.index + result[0].length, file.length);
+    alert("y coordinate" + result);
+
+    parseInventory(str);
+}
+
+function parseInventory(file){
+    var pattern = /[A-z ]+/;
+    var result = pattern.exec(file);
+    str = file.substr(result.index + result[0].length, file.length);
+    alert("item" + result);
+    if(str[3] === '#')
+        parseCell(str);
+    else
+        parseInventory(str);
+}
+
+function parseCell(file){
+    //x position
+    var pattern = /[0-9]+/;
+    var result = pattern.exec(file);
+    var str = file.substr(result.index + result[0].length, file.length);
+    alert("x coordinate " + result);
+
+    //y position
+    pattern = /[0-9]+/;
+    result = pattern.exec(str);
+    str = str.substr(result.index + result[0].length, file.length);
+    alert("y coordinate " + result);
+
+    //visibility
+    pattern = /[0-9]+/;
+    result = pattern.exec(str);
+    str = str.substr(result.index + result[0].length, file.length);
+    alert("visible " + result);
+
+    //terrain id
+    pattern = /[0-9]+/;
+    result = pattern.exec(str);
+    str = str.substr(result.index + result[0].length, file.length);
+    alert("terrain id " + result);
+
+    //content string
+    pattern = /[A-z ]+/;
+    result = pattern.exec(file);
+    str = file.substr(result.index + result[0].length, file.length);
+    alert("content string " + result);
+    parseCell(str);
+}
+
