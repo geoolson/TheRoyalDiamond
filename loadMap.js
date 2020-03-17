0fa33effbc6aadecf043fbbe80c2d75b34d65c50
Geoffrey Olson <geoolson@babbage.cs.pdx.edu>
2020-03-17 15:20:48 -0700

    switched over to json parser for maps

--- loadMap.js
+++ loadMap.js
@@ -1,134 +1,26 @@
 
 //opens file then calls the parser
 var openFile = function(event){
-    var input = event.target;
+  var input = event.target;
 
-    var reader = new FileReader();
-    reader.onload = function(){
-      map = JSON.parse(reader.result);
-      let dim = map.width;
-      let energy = map.hero.energy;
-      let whiffles = map.hero.whiffles;
-      let x = map.hero.x;
-      let y = map.hero.y;
-      game_map = new Map(dim, dim, x, y, energy, whiffles);
-      map.cells.forEach( cell => {
-        game_map.cells[cell.x][cell.y] = new mapCell(cell.x, cell.y, cell.isVisible, cell.terrain, cell.object);
-      });
-      game_map.hero.inventory = map.hero.inventory;
-      game_map.update();
-    };
-    reader.readAsText(input.files[0]);
+  var reader = new FileReader();
+  reader.onload = function(){
+    map = JSON.parse(reader.result);
+    initMap(map);
+  };
+  reader.readAsText(input.files[0]);
 };
 
-
-function dimensions(){
-    var result = parseNum();
-    if(result)
-        playerInfo(result);
-    else
-        alert("invalid file");
-}
-
-function playerInfo(dim){
-    var x = parseNum();
-    var y = parseNum();
-    var energy = parseNum();
-    var whiffles = parseNum();
-    //The text variable is now exactly what it needs to be in order to load the inventory part in...
-    //  because the parseNum function has truncated the text string to be whatever is inside the player's
-    //  inventory.
-    if( areDimensionsValid(dim, x, y) )
-    {
-        game_map = new Map(dim, dim, x, y, energy, whiffles);
-        parseInventory(game_map);
-    }
-}
-
-
-// Compares the integer value of the set dimensions
-// to the set x and y coordinates that the hero is
-// supposed to start at. If the coordinates are not
-// valid, the user is alerted and redirected
-// to the welcome page.
-function areDimensionsValid(dim, x, y){
-
-    // Compare set dimensions to x and y coordinates
-    if(x > dim || y > dim)
-    {
-        alert("Invalid player coordinates. Load a different file or fix the current one.");
-        window.location.replace("index.html");
-    }
-    // The coordinates to dimensions are valid
-    else
-        return true;
-}
-
-
-function parseInventory(game_map){
-    //Get First Inventory Item from the file:
-    var result = parseNextString();
-    game_map.hero.add_item(result);
-    //if delimiter is reached begin parsing the game Cells.
-    if(text[2] === '#')
-        parseCell();
-    else
-        parseInventory(game_map);
-}
-
-
-function parseCell(){
-    text = text.trim();
-    var x = parseNum();
-    var y = parseNum();
-
-    var visible = parseNum();
-    var terrain = parseNum();
-    var object = parseNextString();
-    
-    //checking if eof was reached
-    if(isNaN(x))
-        return;
-    game_map.cells[x][y] = new mapCell(x, y, visible, terrain, object);
-    parseCell();
-}
-
-
-//This function will search for the first time a number appears in the game map string
-//  and will truncate the game map string to ignore everything that occurs before the
-//  first number (including the first number itself).  This function will then return 
-//  the first number in the game map string as an int.
-function parseNum(){
-    var pattern = /[0-9]+/;
-    //If the we have set text to be the game map string, then we execute the following code.
-    if(text){
-        //This sets result to be either null (if the pattern was not found), or a string of 
-        //  what  we are looking for.  It parses text, looking for pattern.  And returns the 
-        //  either a string, or null.  Pattern tells exec that it wants to take the first 
-        //  sequence of numbers from text that appear, and ignore everything else.  In the 
-        //  case of the default Frupal map, the first sequence of numbers that appear is "25".  
-        //  Thus, result would contain: "25"
-        var result = pattern.exec(text);
-        //This takes text - which is the game map string - and takes a substring of it
-        //  and stores that substring back to text.  The substring it takes starts at position
-        //  (index of where result first starts to appear in text offset by the length of result)
-        //  and continues to the length of the whole game map string.  Thus, effectively, anything
-        //  that was written in "text" before result appears, and result itself get discarded from
-        //  the game map string.
-        text = text.slice(result.index + result[0].length, text.length);
-        //This Parses result to an integer (because result is an integer in string form right now)
-        //  and then returns it.
-        return parseInt(result);
-    }
-}
-
-
-//This function will return all the letters (including spaces) of the inventory items.
-function parseNextString(){
-    var pattern = /[A-Z]+/i;
-    if(text){
-        var result = pattern.exec(text);
-        text = text.substr(result.index + result[0].length, text.length);
-        return result[0];
-    }
+function initMap(map){
+  let dim = map.width;
+  let energy = map.hero.energy;
+  let whiffles = map.hero.whiffles;
+  let x = map.hero.x;
+  let y = map.hero.y;
+  game_map = new Map(dim, dim, x, y, energy, whiffles);
+  map.cells.forEach( cell => {
+      game_map.cells[cell.x][cell.y] = new mapCell(cell.x, cell.y, cell.isVisible, cell.terrain, cell.object);
+  });
+  game_map.hero.inventory = map.hero.inventory;
+  game_map.update();
 }
